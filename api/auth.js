6e7e923c8f241ed4413c0615b3896c36853d40d7
Sunny Parkash <sunny.parkash@gmail.com>
2021-09-08 08:08:41 +0530

    changed signature of signin

--- api/auth.js
+++ api/auth.js
@@ -1,12 +1,7 @@
 const authApi = require('../helpers/request-helper')('users')
 
-exports.signIn = async (email, code, mobile, password, context) => {
-    return authApi.post({
-        email: email,
-        code: code,
-        mobile: mobile,
-        password: password
-    }, {
+exports.signIn = async (model, context) => {
+    return authApi.post(model, {
         path: 'signIn'
     }, context)
 }
